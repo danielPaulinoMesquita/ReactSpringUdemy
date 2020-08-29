432fd1702d31804bc98a6fd7952ed0d5cffb2a27
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-08-29 17:57:22 -0300

    8- Implementações feitas para o autenticar e salvarUsuario

--- cursoBackEnd/src/main/java/com/example/curso/service/impl/UsuarioServiceImpl.java
+++ cursoBackEnd/src/main/java/com/example/curso/service/impl/UsuarioServiceImpl.java
@@ -1,11 +1,15 @@
 package com.example.curso.service.impl;
 
+import com.example.curso.exception.ErroAutenticacao;
 import com.example.curso.exception.RegraDeNegocioException;
 import com.example.curso.model.entity.Usuario;
 import com.example.curso.model.repository.UsuarioRepository;
 import com.example.curso.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Transient;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
@@ -15,12 +19,23 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Override
     public Usuario autenticar(String email, String senha) {
-        return null;
+        Optional<Usuario> usuario= usuarioRepository.findByEmail(email);
+        if(!usuario.isPresent()){
+            throw new ErroAutenticacao("Usuário não encontrado para o email informado. ");
+        }
+
+        if (!usuario.get().getSenha().equals(senha)){
+            throw  new ErroAutenticacao("Senha Inválida.");
+        }
+
+        return usuario.get();
     }
 
     @Override
+    @Transient
     public Usuario salvarUsuario(Usuario usuario) {
-        return null;
+        validarEmail(usuario.getEmail());
+        return usuarioRepository.save(usuario);
     }
 
     @Override

ec7d388b0859b8bd70df37da2f90cf8e6cba29cb
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-09-16 02:11:13 -0300

    19- Implementando LancamentoService salvar, atualizar, buscar e deletar.

--- cursoBackEnd/src/main/java/com/example/curso/service/impl/LancamentoServiceImpl.java
+++ cursoBackEnd/src/main/java/com/example/curso/service/impl/LancamentoServiceImpl.java
@@ -4,9 +4,13 @@ import com.example.curso.model.entity.Lancamento;
 import com.example.curso.model.entity.StatusLancamento;
 import com.example.curso.model.repository.LancamentoRepository;
 import com.example.curso.service.LancamentoService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class LancamentoServiceImpl implements LancamentoService {
@@ -18,27 +22,42 @@ public class LancamentoServiceImpl implements LancamentoService {
     }
 
     @Override
+    @Transactional
     public Lancamento salvar(Lancamento lancamento) {
-        return null;
+        return lancamentoRepository.save(lancamento);
     }
 
     @Override
+    @Transactional
     public Lancamento atualizar(Lancamento lancamento) {
-        return null;
+        Objects.requireNonNull(lancamento.getId());
+        return lancamentoRepository.save(lancamento);
     }
 
     @Override
+    @Transactional
     public void deletar(Lancamento lancamento) {
-
+        lancamentoRepository.delete(lancamento);
     }
 
+    /**
+     * Example é uma classe da API do spring que filtra de acordo
+     * com o objeto passsado pra ele
+     * @param lancamentoFiltro
+     * @return
+     */
     @Override
+    @Transactional
     public List<Lancamento> buscar(Lancamento lancamentoFiltro) {
-        return null;
+        Example example= Example.of(lancamentoFiltro,
+                ExampleMatcher.matching().withIgnoreCase().withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+
+        return lancamentoRepository.findAll(example);
     }
 
     @Override
     public void atualizarStatus(Lancamento lancamento, StatusLancamento statusLancamento) {
-
+        lancamento.setStatus(statusLancamento);
+        atualizar(lancamento);
     }
 }

d50074e886599b50a428b0c0146d1a3aeebbcfbb
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-11-16 18:54:11 -0300

    45- Criando a view de Consulta de lançamentos e componente SelectMenu (combo box)

--- curso-front-end/src/main/rotas.js
+++ curso-front-end/src/main/rotas.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
-import Login from "../views/login";
-import CadastroUsuario from "../views/cadastroUsuario";
 import Home from '../views/home'
+import CadastroUsuario from "../views/cadastroUsuario";
+import ConsultaLancamento from "../views/consultaLancamento";
+import Login from "../views/login";
 
 import { Route, Switch, HashRouter } from 'react-router-dom'
 
@@ -11,8 +12,9 @@ function Rotas() {
             <HashRouter>
                 <Switch>
                     <Route path="/home" component={Home}/>
-                    <Route path="/login" component={Login}/>
                     <Route path="/cadastro-usuarios" component={CadastroUsuario}/>
+                    <Route path="/consulta-lancamentos" component={ConsultaLancamento}/>
+                    <Route path="/login" component={Login}/>
                 </Switch>
             </HashRouter>
         )

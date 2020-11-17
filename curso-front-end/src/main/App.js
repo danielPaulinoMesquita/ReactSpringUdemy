aef472eb5ef41540db8010c3473a944c5bf770ec
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-11-17 15:35:21 -0300

    52- Adicionando a biblioteca primeReact

--- curso-front-end/src/main/App.js
+++ curso-front-end/src/main/App.js
@@ -8,6 +8,10 @@ import '../custom.css'
 import 'toastr/build/toastr.min'
 import 'toastr/build/toastr.css'
 
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 class App extends React.Component{
     render() {
         return(

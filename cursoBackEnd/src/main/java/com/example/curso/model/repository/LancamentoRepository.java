9f9a23147de0deea048588dc7dd0c59350399781
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-11-19 01:53:12 -0300

    65- Calculando e apresentado corretamente o saldo na view da home

--- cursoBackEnd/src/main/java/com/example/curso/model/repository/LancamentoRepository.java
+++ cursoBackEnd/src/main/java/com/example/curso/model/repository/LancamentoRepository.java
@@ -1,6 +1,7 @@
 package com.example.curso.model.repository;
 
 import com.example.curso.model.entity.Lancamento;
+import com.example.curso.model.entity.StatusLancamento;
 import com.example.curso.model.entity.enums.TipoLancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +12,10 @@ import java.math.BigDecimal;
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
     @Query(value = "select sum(l.valor) from lancamento l join l.usuario u " +
-            "where u.id = :idUsuario and l.tipo = :tipo group by u")
-    BigDecimal obterSaldoPorTipoLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
+            "where u.id = :idUsuario and l.tipo = :tipo and l.status = :status group by u")
+    BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus(
+            @Param("idUsuario") Long idUsuario,
+            @Param("tipo") TipoLancamento tipo,
+            @Param("status") StatusLancamento status);
 
 }

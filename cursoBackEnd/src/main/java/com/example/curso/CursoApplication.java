f23202b9d14973a2e87a20602621a988448fb009
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-11-19 14:26:13 -0300

    72- Correção nos testes, isolando configuração de CORS da application

--- cursoBackEnd/src/main/java/com/example/curso/CursoApplication.java
+++ cursoBackEnd/src/main/java/com/example/curso/CursoApplication.java
@@ -7,15 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
 public class CursoApplication implements WebMvcConfigurer {
-
-	@Override
-	public void addCorsMappings(CorsRegistry corsRegistry){
-		// addMapping = são os endpoints da api permitidos e allowedOrigins = é a url do front end que vai fazer requisição
-		// corsRegistry.addMapping("/**").allowedOrigins("urlOrigemFront").allowedMethods();
-		corsRegistry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);

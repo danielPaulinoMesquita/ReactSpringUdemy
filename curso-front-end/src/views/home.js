b1d7ee5d7f484fe330c59bfa167ba65ebb6155b4
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-11-19 13:49:32 -0300

    70- Atualizando os componentes de classe para acessar o contexto de autenticaca

--- curso-front-end/src/views/home.js
+++ curso-front-end/src/views/home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import UsuarioService from "../app/service/usuarioService";
-import LocalStorageService from "../app/service/localStorageService";
+import {AuthContext} from "../main/providerAutenticacao";
 
 class Home extends React.Component{
 
@@ -14,7 +14,7 @@ class Home extends React.Component{
     }
 
     componentDidMount() {
-        const usuarioLogado = LocalStorageService.obterItem('_usuario_logado');
+        const usuarioLogado = this.context.usuarioAutenticado;
 
         this.usuarioService
             .obterSaldoPorUsuario(usuarioLogado.id)
@@ -47,4 +47,5 @@ class Home extends React.Component{
     }
 }
 
+Home.contextType = AuthContext;
 export default Home;

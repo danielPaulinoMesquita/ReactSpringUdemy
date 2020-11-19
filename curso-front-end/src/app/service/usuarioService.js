7f1e89a869ef6d988209dafe89052853774672ce
danielPaulinoMesquita <daniel-paulino@hotmail.com>
2020-11-19 00:24:41 -0300

    61- Aplicando Nova Validação para usuários.

--- curso-front-end/src/app/service/usuarioService.js
+++ curso-front-end/src/app/service/usuarioService.js
@@ -1,4 +1,5 @@
 import ApiService from "../api_service";
+import ErroValidacao from "../exception/ErroValidacao";
 
 class UsuarioService extends ApiService{
     constructor() {
@@ -11,6 +12,30 @@ class UsuarioService extends ApiService{
 
     obterSaldoPorUsuario(id){
         return this.get(`/${id}/saldo`)
+    }
+
+    validar(usuario){
+        const erros = [];
+
+        if (!usuario.nome){
+            erros.push('O campo Nome é obrigatório. ')
+        }
+
+        if (!usuario.email){
+            erros.push('O campo Email é obrigatório. ')
+        }else if (!usuario.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)){
+            erros.push('Informe um Email válido. ')
+        }
+
+        if (!usuario.senha || !usuario.senhaRepeticao){
+            erros.push('Digite a senha 2x. ')
+        }else if(usuario.senha !== usuario.senhaRepeticao){
+            erros.push('As senhas não convergem')
+        }
+
+        if(erros && erros.length > 0){
+            throw new ErroValidacao(erros);
+        }
     }
 
     salvar(usuario){
